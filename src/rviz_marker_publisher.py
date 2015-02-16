fb3de6238f1a0e6556b5686f9a2029379507d2a4
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-02-16 14:57:47 -0800

    Add tf broadcaster to rviz_marker_publisher

--- src/rviz_marker_publisher.py
+++ src/rviz_marker_publisher.py
@@ -3,7 +3,7 @@
 """
 usage:
 rosrun apc rviz_marker_publisher.py \
-    -n "rviz_marker_publisher" -t 1 -r
+    -n "rviz_marker_publisher" -t 1 -r \
     -m "/home/dibyo/workspace/amazon_picking_challenge/expo_dry_erase_board_eraser/meshes/poisson.stl" \
     -p "[0, 0, 0]" -o "[0, 0, 0, 1]"
 """
@@ -65,10 +65,7 @@ class RvizPointsPublisher(ROSNode):
             self.GRIPPER: self.refresh_marker_gripper
         }
 
-        if self.mesh_file:
-            self.refresh_marker_mesh()
-        else:
-            self.refresh_marker_arrow()
+        self.refresh_marker_map[self.marker_type]()
 
     ARROW = 0
     MESH = 1
@@ -92,12 +89,12 @@ class RvizPointsPublisher(ROSNode):
         '3': ((0, 0, 0), (0, 0, 1), 0),
 
         # Gripper width controls
-        'g': ((0, 0, 0), (0, 0, 0), 1),
-        'y': ((0, 0, 0), (0, 0, 0), -1)
+        'g': ((0, 0, 0), (0, 0, 0), -1),
+        'y': ((0, 0, 0), (0, 0, 0), 1)
     }
 
     def update_pose(self, move):
-        delta = self.move_map.get(move, ((0, 0, 0), (0, 0, 0)))
+        delta = self.move_map.get(move, ((0, 0, 0), (0, 0, 0), 0))
 
         # Position change
         self.pose.position.x += self.speed * delta[0][0]
@@ -115,7 +112,7 @@ class RvizPointsPublisher(ROSNode):
         self.pose.orientation = Quaternion(*quaternion_from_euler(roll, pitch, yaw))
 
         # Gripper width change
-        self.gripper_width += delta[2] * 0.001
+        self.gripper_width += delta[2] * 0.005
 
     @staticmethod
     def create_marker_msg(pose, type, id, frame_id='base_link', ns='apc',
@@ -147,15 +144,16 @@ class RvizPointsPublisher(ROSNode):
 
         return marker
 
-    def publish_marker_msg_and_transform(self, marker):
+    def publish_marker_msg(self, marker):
         while not self.pub.get_num_connections():
             rospy.sleep(0.1)
 
-        print "publishing marker: \n{}".format(str(marker))
+        # print "publishing marker: \n{}".format(str(marker))
         self.pub.publish(marker)
 
-        p = marker.pose.position
-        o = marker.pose.orientation
+    def broadcast_transform(self):
+        p = self.pose.position
+        o = self.pose.orientation
         self.br.sendTransform((p.x, p.y, p.z), (o.x, o.y, o.z, o.w),
                               rospy.Time.now(), self.name, "base_link")
 
@@ -168,7 +166,8 @@ class RvizPointsPublisher(ROSNode):
                                         scale_x=self.scale, scale_y=self.scale,
                                         scale_z=self.scale)
 
-        self.publish_marker_msg_and_transform(marker)
+        self.publish_marker_msg(marker)
+        self.broadcast_transform()
 
     def refresh_marker_arrow(self):
         """
@@ -180,27 +179,30 @@ class RvizPointsPublisher(ROSNode):
                                         scale_y=self.scale*0.2,
                                         scale_z=self.scale*0.2)
 
-        self.publish_marker_msg_and_transform(marker)
+        self.publish_marker_msg(marker)
+        self.broadcast_transform()
 
     def refresh_marker_gripper(self):
         marker = self.create_marker_msg(pose=self.pose, type=Marker.CUBE_LIST,
                                         action=Marker.ADD, id=self.id,
-                                        scale_x=self.scale*0.05,
+                                        scale_x=self.scale*0.1,
                                         scale_y=self.scale*0.01,
-                                        scale_z=self.scale*0.02)
+                                        scale_z=self.scale*0.05)
 
         marker.points = [
             Point(0, -self.gripper_width/2, 0),
-            Point(0, self.gripper_width/2, 0)
+            Point(0, self.gripper_width/2, 0),
+            Point(-0.075, 0, 0)
         ]
 
-        self.publish_marker_msg_and_transform(marker)
+        self.publish_marker_msg(marker)
+        self.broadcast_transform()
 
     def delete_marker(self):
         marker = self.create_marker_msg(pose=self.pose, type=0,
                                         action=Marker.DELETE, id=self.id)
 
-        self.publish_marker_msg_and_transform(marker)
+        self.publish_marker_msg(marker)
 
     def enter_control_loop(self):
         string = "This was typed: "
@@ -224,12 +226,12 @@ if __name__ == '__main__':
     # parser.add_argument('-i', '--id', default=-1, type=int)
     parser.add_argument('-n', '--name', default='rviz_marker')
     parser.add_argument('-r', '--remove', action='store_true',
-                        help='whether the object is removed on node exit')
+                        help='whether the object should be removed on node exit')
     parser.add_argument('-t', '--type', default='0', choices={0, 1, 2},
                         help='{0}: ARROW, {1}: MESH, {2}: GRIPPER'.
                         format(RvizPointsPublisher.ARROW,
                                RvizPointsPublisher.MESH,
-                               RvizPointsPublisher.GRIPPER))
+                               RvizPointsPublisher.GRIPPER), type=int)
     parser.add_argument('-p', '--position', default='[0, 0, 0]',
                         type=yaml.load)
     parser.add_argument('-o', '--orientation', default='[0, 0, 0, 1]',
@@ -244,19 +246,5 @@ if __name__ == '__main__':
     publisher = RvizPointsPublisher(marker_type=args.type, name=args.name,
                                     pose=pose, mesh_file=args.mesh_file,
                                     remove=args.remove)
-    # import sys
-    # pose = Pose()
-    # pose.position = Point(0, 0, 0)
-    # pose.orientation = Quaternion(0, 0, 0, 1)
-    # mesh_location = ""
-    #
-    # if len(sys.argv) >= 2:
-    #     mesh_location = sys.argv[1]
-    # if len(sys.argv) >= 3:
-    #     pose.position = Point(*yaml.load(sys.argv[2]))
-    # if len(sys.argv) >= 4:
-    #     pose.orientation = Quaternion(*yaml.load(sys.argv[3]))
-    #
-    # publisher = RvizPointsPublisher("rviz_points_publisher", pose, mesh_location)
 
-    print publisher.enter_control_loop()
+    publisher.enter_control_loop()
