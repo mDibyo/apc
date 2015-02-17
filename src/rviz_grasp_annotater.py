76b37f84d4a2f5d112f132336d7d6ef1fd9d95a4
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-02-16 17:43:12 -0800

    Add basic implementation for rviz_grasp_annotater

--- src/rviz_grasp_annotater.py
+++ src/rviz_grasp_annotater.py
@@ -6,64 +6,102 @@ roslib.load_manifest('apc')
 import rospy
 import tf
 from visualization_msgs.msg import Marker
+from geometry_msgs.msg import Pose, Point, Quaternion
+
+import os.path as osp
+import json
 
 from ros_utils import TopicSubscriberNode, ROSNode
+from rviz_marker_publisher import RvizMarkerPublisher
+
 
 
 __author__ = 'dibyo'
 
+DATA_DIRECTORY = osp.abspath("../data")
 
-# class RvizGraspAnnotator(ROSNode):
-#     def __init__(self, gripper_marker_id, object_marker_id):
-#         super(RvizGraspAnnotator, self).__init__('rviz_grasp_annotator')
-#
-#
-#         gripper_marker_listener = TopicSubscriberNode('gripper_marker_listener',
-#                                                       'visualization_marker',
-#                                                       Marker, anonymous=True)
-#         gripper_marker_listener.set_msg_filter(lambda msg:
-#                                                msg.id == gripper_marker_id)
-#         gripper_marker_listener.add_callback()
-#
-#         object_marker_listener = TopicSubscriberNode('object_marker_listener',
-#                                                      'visualization_marker',
-#                                                      Marker, anonymous=True)
-#         object_marker_listener.set_msg_filter(lambda msg:
-#                                               msg.id == object_marker_id)
-#         object_marker_listener.add_callback()
-#
-#
-#
-#     def get_gripper_transform(self, wait=False):
-#         """
-#         Get gripper transform with respect to object
-#
-#         :param wait: whether this method should wait till transforms are available
-#         """
+
+class Grasp(object):
+    def __init__(self, pose, gripper_width):
+        self.pose = pose
+        self.gripper_width = gripper_width
+
+    def to_json(self):
+        p = self.pose.position
+        o = self.pose.orientation
+
+        d = {
+            'pose': {
+                'position': {'x': p.x, 'y': p.y, 'z': p.z},
+                'orientation': {'x': o.x, 'y': o.y, 'z': o.z, 'w': o.w}
+            },
+            'gripper_width': self.gripper_width
+        }
+        return d
+
+    @classmethod
+    def from_json(cls, d):
+        pose = Pose()
+        pose.position = Point(**d['pose']['position'])
+        pose.orientation = Quaternion(**d['pose']['orientation'])
+
+        return cls(pose, d['gripper_width'])
+
+
+class RvizMarkerPublisherAugmented(RvizMarkerPublisher):
+
+    def __init__(self, *args, **kwargs):
+        super(RvizMarkerPublisherAugmented, self).__init__(*args, **kwargs)
+        self.grasps = []
+
+    def update_pose(self, move):
+        if move == ' ':
+            self.grasps += Grasp(self.pose, self.gripper_width)
+
+        super(RvizMarkerPublisherAugmented, self).update_pose(move)
+
+
 class RvizGraspAnnotator(ROSNode):
-    def __init__(self, name, gripper_marker, object_marker):
-        super(RvizGraspAnnotator, self).__init__(name)
-
-        self.gripper_marker = gripper_marker
-        self.object_marker = object_marker
+    def __init__(self, object_name):
+        super(RvizGraspAnnotator, self).__init__('rviz_grasp_annotator',
+                                                 anonymous=False)
+        self.object_name = object_name
 
         self.listener = tf.TransformListener()
 
-        print self.get_gripper_transform()
+        mesh_file = osp.join(DATA_DIRECTORY, 'meshes', self.object_name)
+        self.object_marker = RvizMarkerPublisher(marker_type=1,
+                                                 name='object_marker',
+                                                 pose=Pose(),
+                                                 mesh_file=mesh_file,
+                                                 remove=True)
 
-    def get_gripper_transform(self):
-        trans, rot = self.listener.lookupTransform(self.gripper_marker,
-                                                   self.object_marker,
-                                                   rospy.Time(0))
-        print trans, rot
+        self.gripper_marker = RvizMarkerPublisherAugmented(marker_type=2,
+                                                           name='gripper_marker',
+                                                           pose=Pose(),
+                                                           remove=True)
 
-if __name__ == "__main__":
-    # TODO: Get this info from command line
-    gripper_marker = ''
-    object_marker = ''
+    def __enter__(self):
+        grasps_file = osp.join(DATA_DIRECTORY, 'grasps', self.object_name)
 
-    node = RvizGraspAnnotator('grasp_annotator', gripper_marker, object_marker)
-    node.spin()
+        if osp.exists(grasps_file):
+            with open(grasps_file, 'r') as f:
+                self.gripper_marker.grasps = \
+                    [Grasp.from_json(grasp_json) for grasp_json in json.load(f)]
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        grasps_file = osp.join(DATA_DIRECTORY, 'grasps', self.object_name)
+        with open(grasps_file, 'w') as f:
+            json.dump([grasp.to_json() for grasp in self.gripper_marker.grasps], f)
+
+    def enter_control_loop(self):
+        pass
+
+
+
+
 
 
 
