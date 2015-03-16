72427d004606dfd5dd1ad97c3e1bf331d17843cf
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-03-16 08:13:39 -0700

    Delete old unused interactive marker nodes

--- src/rviz_visualization_marker_listener.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'dibyo'
-
-
-from ros_utils import TopicSubscriberNode
-
-if __name__ == "__main__":
-    node = TopicSubscriberNode("apc_object_listener",
-                               "apc/update",
-                               )
