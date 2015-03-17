1fce72191e3f4b1b07426d9c0a302a7d4b6b708c
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-03-16 23:02:32 -0700

    Add PR2 control

--- src/apc_controller.py
+++ src/apc_controller.py
@@ -18,22 +18,59 @@ __author__ = 'dibyo'
 class APCController(ROSNode):
     def __init__(self, joint_trajectories_topic, exec_status_topic):
         super(APCController, self).__init__('apc_controller')
-
         self.joint_trajectories_topic = joint_trajectories_topic
         self.exec_status_topic = exec_status_topic
 
-        self.robot_exec_status = None
+        self._robot_exec_status = None
 
         self.get_motion_plan_client = rospy.ServiceProxy('get_motion_plan',
                                                          GetMotionPlan)
+
         self.joint_trajectories_publisher = rospy.Publisher(self.joint_trajectories_topic,
                                                             JointTrajectory)
         self.exec_status_subscriber = rospy.Subscriber(self.exec_status_topic,
                                                        ExecStatus,
                                                        self.record_exec_status)
 
+    @property
+    def robot_exec_status(self):
+        return self._robot_exec_status
+
+    @robot_exec_status.setter
+    def robot_exec_status(self, exec_status):
+        if exec_status == ExecStatus.ERROR:
+            rospy.logerr("Error in robot execution status")
+        self._robot_exec_status = exec_status
+
     def record_exec_status(self, status):
         self.robot_exec_status = status
 
-    def execute_strategy(self, strategy):
-        pass
+    def wait_for_busy_exec_status(self):
+        while self.robot_exec_status != ExecStatus.BUSY:
+            rospy.sleep(0.1)
+        rospy.loginfo("robot execution status busy")
+
+    def wait_for_idle_exec_status(self):
+        while self.robot_exec_status != ExecStatus.IDLE:
+            rospy.sleep(0.1)
+        rospy.loginfo("robot execution status idle")
+
+    def execute_motion_plan(self, motion_plan):
+        for joint_trajectory in motion_plan.trajectories:
+            self.joint_trajectories_publisher.publish(joint_trajectory)
+            # self.wait_for_busy_exec_status()
+            rospy.sleep(0.1)
+            self.wait_for_idle_exec_status()
+
+    def execute_work_order(self, work_order):
+        try:
+            res = self.get_motion_plan_client(work_order)
+        except rospy.ServiceException as e:
+            rospy.logerr("Service call failed: {}".format(e))
+
+
+if __name__ == '__main__':
+    controller = APCController('joint_trajectories', 'exec_status')
+    work_order = BinWorkOrder('broad_tall', ['dove_beauty_bar'],
+                              'dove_beauty_bar', 'simple')
+    controller.execute_work_order(work_order)
--- src/apc_trajectory_executor.py
+++ src/apc_trajectory_executor.py
@@ -5,10 +5,13 @@ from __future__ import division
 import roslib
 roslib.load_manifest('apc')
 import rospy
+from std_msgs.msg import Header
 from trajectory_msgs.msg import JointTrajectory
+from pr2.arm import Arm
 
 from ros_utils import ROSNode
 from apc.msg import ExecStatus
+from utils import LoopingThread
 
 
 __author__ = 'dibyo'
@@ -20,11 +23,47 @@ class APCTrajectoryExecutor(ROSNode):
 
         self.joint_trajectories_topic = joint_trajectories_topic
         self.exec_status_topic = exec_status_topic
+        self.exec_status = ExecStatus.IDLE
+
+        self.larm = Arm('left')
+        self.larm_gripper_name = 'l_gripper_l_finger_joint'
+        self.rarm = Arm('right')
+        self.rarm_gripper_name = 'r_gripper_l_finger_joint'
 
         self.joint_trajectories_subscriber = rospy.Subscriber(self.joint_trajectories_topic,
                                                               JointTrajectory,
                                                               self.execute_joint_trajectory)
+
         self.exec_status_publisher = rospy.Publisher(self.exec_status_topic, ExecStatus)
+        self.exec_status_publisher_thread = LoopingThread(target=self.publish_exec_status,
+                                                          rate=50)
+        self.exec_status_publisher_thread.start()
+
+    def publish_exec_status(self):
+        header = Header()
+        header.stamp(rospy.Time.now())
+        self.exec_status_publisher.publish(ExecStatus(header, self.exec_status))
 
     def execute_joint_trajectory(self, trajectory):
-        pass
+        # TODO: Add handling for speed
+
+        try:
+            joint_names = trajectory.joint_names
+            if joint_names[0][0] == 'l':
+                arm = self.larm
+            elif joint_names[0][0] == 'r':
+                arm = self.rarm
+            else:  # TODO: Add handling for torso joint
+                return
+
+            if len(joint_names) == 1:
+                if trajectory.points[0][0] == 0:
+                    arm.close_gripper(block=False)
+                else:
+                    arm.open_gripper(block=False)
+            elif len(joint_names) > 1:
+                self.exec_status = ExecStatus.BUSY
+                arm.execute_joint_trajectory(trajectory.points)
+                self.exec_status = ExecStatus.IDLE
+        except rospy.ROSException:
+            self.exec_status = ExecStatus.ERROR
