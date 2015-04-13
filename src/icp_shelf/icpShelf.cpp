d3498ea3951c5a6e9ef3c866e3996c511ba885d9
Nikhil Mishra <nmishra@berkeley.edu>
2015-04-13 16:32:16 -0700

    more on icp

--- src/icp_shelf/icpShelf.cpp
+++ src/icp_shelf/icpShelf.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <pcl/io/pcd_io.h>
-#include <pcl/io/ply_io.h>
 #include <pcl/point_types.h>
 #include <pcl/point_cloud.h>
 #include <pcl/filters/filter.h>
@@ -16,6 +15,7 @@ int main(int argc, char** argv) {
       PCL_ERROR ("Couldn't read file\n");
       return -1;
   }
+
   std::cout << "Loaded cloud with " << cloudRaw->points.size() << " points." << std::endl;
   
   std::cout << "Removing NaN's..." << std::endl;
@@ -23,13 +23,13 @@ int main(int argc, char** argv) {
   std::vector<int> indices;
   pcl::removeNaNFromPointCloud(*cloudRaw, *cloudTrim, indices);
   std::cout << "Trimmed cloud has " << cloudTrim->points.size() << " points." << std::endl;
-  
-  pcl::io::savePLYFile("trimmed.ply", *cloudTrim);
-  std::cout << "wrote trimmed cloud to file" << std::endl;
-  
+
   std::cout << "Loading shelf model..." << std::endl;
   PointCloud::Ptr cloudShelf (new PointCloud);
-  pcl::io::loadPLYFile(argv[2], *cloudShelf);
+  if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[2], *cloudShelf) == -1) {
+      PCL_ERROR ("Couldn't read file\n");
+      return -1;
+  }
   std::cout << "Loaded shelf cloud with " << cloudShelf->points.size() << " points." << std::endl;
   
   std::cout << "Beginning ICP..." << std::endl;
@@ -41,4 +41,5 @@ int main(int argc, char** argv) {
   std::cout << "Convergence:" << icp.hasConverged() << ", Score: " << icp.getFitnessScore() << std::endl;
   std::cout << icp.getFinalTransformation() << std::endl;
   
+
 }
